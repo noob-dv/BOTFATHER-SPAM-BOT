3c052762eceb9f438ed49d90da6fbf827bb7585b
♡мιcкεү♡ <88245294+MickeyxD@users.noreply.github.com>
2021-11-05 23:31:21 +0530

    Update bigspam.py

--- Botfather/plugins/bigspam.py
+++ Botfather/plugins/bigspam.py
@@ -40,11 +40,11 @@ for x in SUDO_USERS:
 async def spam(e):
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
-        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        mickey = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         smex = await e.get_reply_message()
         if len(legend) == 2:
-            message = str(legend[1])
-            counter = int(legend[0])
+            message = str(mickey[1])
+            counter = int(mickey[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     if e.reply_to_msg_id:
@@ -53,7 +53,7 @@ async def spam(e):
                         await e.client.send_message(e.chat_id, message)
                     await asyncio.sleep(0.1)
         elif e.reply_to_msg_id and smex.media:
-            counter = int(legend[0])
+            counter = int(mickey[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "document"):
                     smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
